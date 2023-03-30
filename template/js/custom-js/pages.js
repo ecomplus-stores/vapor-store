7acc054da69ff0dc144a393c16ca397de9191269
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-03-30 20:12:34 -0300

    chore: insert modal information

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,7 @@
 // Add your custom JavaScript for storefront pages here.
+setTimeout(() => {
+    document.getElementById('closeModal').addEventListener('click', () => sessionStorage.setItem('modal', 'close'))
+    if (!sessionStorage.getItem('modal')) {
+        $('#exampleModalLong').modal('show')
+    }
+}, 1200)
