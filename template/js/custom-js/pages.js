0ee8059725558fca9729db2d88a252ea05e1d3ea
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-05-17 16:31:31 -0300

    chore: remove popup and update store

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,7 +1,1 @@
 // Add your custom JavaScript for storefront pages here.
-setTimeout(() => {
-    document.getElementById('closeModal').addEventListener('click', () => sessionStorage.setItem('modal', 'close'))
-    if (!sessionStorage.getItem('modal')) {
-        $('#exampleModalLong').modal('show')
-    }
-}, 1200)
