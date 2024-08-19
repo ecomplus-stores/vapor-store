bc93b764158dccd55aa824ffa0d709385dd33c81
Vitor Rocha Gonçalves <35348281+vitorrgg@users.noreply.github.com>
2024-08-19 14:15:35 -0300

    Update checkout.js

--- template/js/checkout.js
+++ template/js/checkout.js
@@ -41,7 +41,7 @@ setInterval(function () {
       var countQuantity = ecomCart.data.subtotal
       if (countQuantity < lessQuantity) {
         window.location.href = '/app/#/cart'
-        window.alert('Valor mínimo de pedido é R$500, insira mais itens')
+        window.alert('Um dos produtos o estoque acabou! Você estará sendo direcionado para o carrinho para inserir mais itens para pedido mínimo')
       }
     }
 
@@ -57,3 +57,125 @@ setInterval(function () {
     router1.afterEach(addRoute1ToData)
   }
 }, 300)
+
+/* import '#template/js/checkout'
+import './custom-js/checkout'
+import ecomCart from '@ecomplus/shopping-cart'
+var lessUnit = document.getElementById('lessUnit')
+var firstphrase = document.getElementById('lessSome')
+var lastphrase = document.getElementById('noMore')
+var lessQuantity = 0.01
+lessUnit.innerHTML = window.ecomUtils.formatMoney(lessQuantity, 'BRL', 'pt_br')
+ecomCart.on('change', ({ data }) => {
+  var cartCalc = document.querySelectorAll('#cart')
+  if (cartCalc.length) {
+    document.getElementById('containerCalc').style.display = 'block'
+    var checkoutButton = document.querySelector('.cart__btn-checkout')
+    var percentBar
+    var countQuantity = data.subtotal
+    var evalQuantity = lessQuantity - countQuantity
+    if (evalQuantity > 0) {
+      lessUnit.innerHTML = window.ecomUtils.formatMoney(evalQuantity, 'BRL', 'pt_br')
+      percentBar = Math.round(countQuantity / lessQuantity * 100) + '%'
+      document.getElementById('lastUnitsBar').style.width = percentBar
+      document.getElementById('percentBar').innerHTML = percentBar
+      firstphrase.style.display = 'block'
+      lastphrase.style.display = 'none'
+      checkoutButton.style.display = 'none'
+    } else {
+      percentBar = '100%'
+      checkoutButton.style.display = 'block'
+      firstphrase.style.display = 'none'
+      lastphrase.style.display = 'block'
+      document.getElementById('lastUnitsBar').style.width = percentBar
+      document.getElementById('percentBar').innerHTML = percentBar
+    }
+  } else {
+    document.getElementById('containerCalc').style.display = 'none'
+  }
+})
+const router1 = window.storefrontApp && window.storefrontApp.router
+setInterval(function () {
+  if (router1) {
+    const emitCheckout1 = (name) => {
+      var countQuantity = ecomCart.data.subtotal
+      if (countQuantity < lessQuantity) {
+        window.location.href = '/app/#/cart'
+        window.alert('Um dos produtos o estoque acabou! Você estará sendo direcionado para o carrinho para inserir mais itens para pedido mínimo')
+      }
+    }
+
+    const addRoute1ToData = ({ name }) => {
+      if (name === 'checkout') {
+        emitCheckout1(name)
+      }
+    }
+
+    if (router1.currentRoute) {
+      addRoute1ToData(router1.currentRoute)
+    }
+    router1.afterEach(addRoute1ToData)
+  }
+}, 300) 
+
+import '#template/js/checkout'
+import './custom-js/checkout'
+import ecomCart from '@ecomplus/shopping-cart'
+var lessUnit = document.getElementById('lessUnit')
+var firstphrase = document.getElementById('lessSome')
+var lastphrase = document.getElementById('noMore')
+var lessQuantity = 0
+lessUnit.innerHTML = lessQuantity
+ecomCart.on('change', ({ data }) => {
+  var cartCalc = document.querySelectorAll('#cart')
+  if (cartCalc.length) {
+    document.getElementById('containerCalc').style.display = 'block'
+    var checkoutButton = document.querySelector('.cart__btn-checkout')
+    var percentBar
+    var countQuantity = data.items.reduce((acc, curr) => acc + curr.quantity, 0)
+    var evalQuantity = lessQuantity - countQuantity
+    if (evalQuantity > 0) {
+      lessUnit.innerHTML = evalQuantity
+      percentBar = Math.round(countQuantity / lessQuantity * 100) + '%'
+      document.getElementById('lastUnitsBar').style.width = percentBar
+      document.getElementById('percentBar').innerHTML = percentBar
+      firstphrase.style.display = 'block'
+      lastphrase.style.display = 'none'
+      checkoutButton.style.display = 'none'
+    } else {
+      percentBar = '100%'
+      checkoutButton.style.display = 'block'
+      firstphrase.style.display = 'none'
+      lastphrase.style.display = 'block'
+      document.getElementById('lastUnitsBar').style.width = percentBar
+      document.getElementById('percentBar').innerHTML = percentBar
+    }
+  } else {
+    document.getElementById('containerCalc').style.display = 'none'
+  }
+})
+const router1 = window.storefrontApp && window.storefrontApp.router
+setInterval(function () {
+  if (router1) {
+    const emitCheckout1 = (name) => {
+      var countQuantity = ecomCart.data.items.reduce((acc, curr) => acc + curr.quantity, 0)
+      if (countQuantity < lessQuantity) {
+       window.location.href = '/app/#/cart'
+       window.alert('Apenas ' + countQuantity + ' itens no carrinho. Um deles acabou estoque! Você estará sendo direcionado para o carrinho para inserir mais um item')
+      }
+    }
+
+    const addRoute1ToData = ({ name }) => {
+      if (name === 'checkout') {
+        emitCheckout1(name)
+      }
+    }
+
+    if (router1.currentRoute) {
+      addRoute1ToData(router1.currentRoute)
+    }
+    router1.afterEach(addRoute1ToData)
+  }
+}, 300)
+
+*/
