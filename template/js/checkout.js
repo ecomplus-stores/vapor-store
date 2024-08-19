c69a17a571e21b129739a83a95b134f7f638099f
vaporstore <78297830+vaporstore@users.noreply.github.com>
2024-08-19 16:26:20 +0200

    Update checkout.js

--- template/js/checkout.js
+++ template/js/checkout.js
@@ -4,7 +4,7 @@ import ecomCart from '@ecomplus/shopping-cart'
 var lessUnit = document.getElementById('lessUnit')
 var firstphrase = document.getElementById('lessSome')
 var lastphrase = document.getElementById('noMore')
-var lessQuantity = 500
+var lessQuantity = 100
 lessUnit.innerHTML = window.ecomUtils.formatMoney(lessQuantity, 'BRL', 'pt_br')
 ecomCart.on('change', ({ data }) => {
   var cartCalc = document.querySelectorAll('#cart')
